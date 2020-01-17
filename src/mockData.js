7dcad52b77175b14c54e18870ea4da7201db302d
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-17 13:40:43 -0300

    Reversed json change to avoid git-push error.

---
+++ src/mockData.js
@@ -0,0 +1,52 @@
+const tipStates = [
+  {
+    title: "Does diet influence mental health?",
+    media: "https://cdn-prod.medicalnewstoday.com/content/images/articles/327/327422/woman-carrying-vegetables.jpg",
+    avatar: "https://pbs.twimg.com/profile_images/1121781998442418177/18ko51Y8_400x400.jpg",
+    id: 234001,
+  },
+  {
+    title: "3 Exercises That Will Prevent Back Pain",
+    media: "https://medlineplus.gov/images/BackPain.jpg",
+    avatar: "https://pbs.twimg.com/profile_images/1199686661954818050/IRXOiWJY_400x400.jpg",
+    id: 234002,
+  },
+  {
+    title: "10 Foods High in Omega-6",
+    media: "https://post.healthline.com/wp-content/uploads/2019/12/walnuts-bowl-nuts-1296x728-header-1296x728.jpg",
+    avatar: "https://pbs.twimg.com/profile_images/1192230741339508736/MN9YwaHD_400x400.jpg",
+    id: 234003,
+  },
+  {
+    title: "Specific exercises linked to cognitive performance",
+    media: "https://scdn.slashgear.com/wp-content/uploads/2020/01/shoes_exercise_main_uns-1280x720.jpg",
+    avatar: "https://pbs.twimg.com/profile_images/1195422183742464002/tj2kKTv8_400x400.jpg",
+    id: 234004,
+  },
+  {
+    title: "Does diet influence mental health?",
+    media: "https://cdn-prod.medicalnewstoday.com/content/images/articles/327/327422/woman-carrying-vegetables.jpg",
+    avatar: "https://pbs.twimg.com/profile_images/1121781998442418177/18ko51Y8_400x400.jpg",
+    id: 234005,
+  },
+  {
+    title: "10 Foods High in Omega-6",
+    media: "https://post.healthline.com/wp-content/uploads/2019/12/walnuts-bowl-nuts-1296x728-header-1296x728.jpg",
+    avatar: "https://pbs.twimg.com/profile_images/1192230741339508736/MN9YwaHD_400x400.jpg",
+    id: 234006,
+  },
+  {
+    title: "Specific exercises linked to cognitive performance",
+    media: "https://scdn.slashgear.com/wp-content/uploads/2020/01/shoes_exercise_main_uns-1280x720.jpg",
+    avatar: "https://pbs.twimg.com/profile_images/1195422183742464002/tj2kKTv8_400x400.jpg",
+    id: 234007,
+  },
+  {
+    title: "3 Exercises That Will Prevent Back Pain",
+    media: "https://medlineplus.gov/images/BackPain.jpg",
+    avatar: "https://pbs.twimg.com/profile_images/1199686661954818050/IRXOiWJY_400x400.jpg",
+    id: 234008,
+  },
+];
+
+export default tipStates;
