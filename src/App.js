465b8d633e94b2b01422127f58464d54785d4753
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-16 22:12:35 -0300

    Deconstructed props object from Tip component.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { create } from "jss";
 import { StylesProvider, jssPreset } from "@material-ui/core/styles";
 
-import "./utils/styles/styles.scss";
+import "./styles/styles.scss";
 import tipState from "./mockData";
 import ListTips from "./components/ListTips/ListTips.jsx";
 
 const jss = create({
   ...jssPreset(),
   // Define a custom insertion point that JSS will look for when injecting the styles into the DOM.
-  insertionPoint: "jss-insertion-point",
+  insertionPoint: document.getElementById("jss-insertion-point"),
 });
 
 function App() {
