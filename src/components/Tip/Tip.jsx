74e363f222ed7dee7eb99251dff28c91c6fc500b
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-16 22:12:35 -0300

    Deconstructed props object from Tip component.

--- src/components/Tip/Tip.jsx
+++ src/components/Tip/Tip.jsx
@@ -3,14 +3,14 @@ import { Avatar, Card, CardContent, CardMedia, Typography } from "@material-ui/c
 
 // import "./Tip.scss";
 
-const Tip = {props} => {
+const Tip = ({ tipState: { avatar, media, title } }) => {
   return (
     <Card square className="card" elevation={0}>
-      <Avatar alt="" className="avatar" src={props.tipState.avatar} />
-      <CardMedia className="media" component="img" image={props.tipState.media} title="" />
+      <Avatar alt="" className="avatar" src={avatar} />
+      <CardMedia className="media" component="img" image={media} title="" />
       <CardContent className="cardText">
         <Typography className="title" component="h2" variant="subtitle2">
-          {props.tipState.title}
+          {title}
         </Typography>
       </CardContent>
     </Card>
