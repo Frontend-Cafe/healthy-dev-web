966b2ef7e0a74368f19a8257c64e6170fd6010b5
Victor Escalona <victorescalonag@gmail.com>
2020-01-19 17:05:01 -0500

    Login Component done

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,29 +4,33 @@ import "../utils/styles/Login.css";
 import { FilledInput } from "@material-ui/core";
 
 function Login() {
-    return (
-        <div className="container">
-            <div className="title_login">
-                Bienvenide a <br /> Healthy Dev App
+  return (
+    <div className="container">
+      <div className="title_login">
+        Bienvenide a <br /> Healthy Dev App
       </div>
-            <div className="login_form">
-                <form action="">
-                    <input className="input__info" placeholder="Email" type="email" />
-                    <p className="assistive_text">Assistive text</p>
-                    <input className="input__info" placeholder="Password" type="password" />
-                    <p className="assistive_text">Assistive text</p>
-                    <input className="btn_submit" type="submit" value="LOGIN" />
-                    <Link className="forgot_pass" to={"/"}>
-                        ¿Olvidaste tu Password?
+      <div className="login_form">
+        <form action="">
+          <input className="input__info" placeholder="Email" type="email" />
+          <p className="assistive_text">Assistive text</p>
+          <input
+            className="input__info"
+            placeholder="Password"
+            type="password"
+          />
+          <p className="assistive_text">Assistive text</p>
+          <input className="btn_submit" type="submit" value="LOGIN" />
+          <Link className="forgot_pass" to={"/"}>
+            ¿Olvidaste tu Password?
           </Link>{" "}
-                    <br />
-                    <Link className="signup" to={"/"}>
-                        CREAR CUENTA
+          <br />
+          <Link className="signup" to={"/"}>
+            CREAR CUENTA
           </Link>
-                </form>
-            </div>
-        </div>
-    );
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
