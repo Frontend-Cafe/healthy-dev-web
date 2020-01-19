86f4617dae695b1d924ef10a3544d6e796a44ab2
Victor Escalona <victorescalonag@gmail.com>
2020-01-19 17:02:11 -0500

    Login Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Login from "./components/Login";
+
 function App() {
   return (
     <div className="App">
       <Router>
-        
         <Route exact path="/" />
+        <Route exact component={Login} path="/login" />
       </Router>
     </div>
   );
