c30a801153bb5b6497fce3c59d268f689c643246
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-20 18:48:35 -0300

    Made corrections from PR

--- src/components/Tip/Tip.jsx
+++ src/components/Tip/Tip.jsx
@@ -4,7 +4,7 @@ import { Avatar, Card, CardContent, CardMedia, Typography } from "@material-ui/c
 const Tip = ({ tipState: { avatar, media, title } }) => {
   return (
     <Card square className="card" elevation={0}>
-      <Avatar alt="" className="avatar" src={avatar} />
+      <Avatar alt="" src={avatar} />
       <CardMedia className="media" component="img" image={media} title="" />
       <CardContent className="cardText">
         <Typography className="title" component="h2" variant="subtitle2">
