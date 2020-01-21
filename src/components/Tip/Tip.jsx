d1d8ed9b0f5bef1ff0263a6d6885273086083d9e
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-21 19:21:44 -0300

    Removed return from comps. Cleared margin.

--- src/components/Tip/Tip.jsx
+++ src/components/Tip/Tip.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Avatar, Card, CardContent, CardMedia, Typography } from "@material-ui/core";
 
-const Tip = ({ tipState: { avatar, media, title } }) => {
-  return (
-    <Card square className="card" elevation={0}>
-      <Avatar alt="" src={avatar} />
-      <CardMedia className="media" component="img" image={media} title="" />
-      <CardContent className="cardText">
-        <Typography className="title" component="h2" variant="subtitle2">
-          {title}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-};
+const Tip = ({ tipState: { avatar, media, title } }) => (
+  <Card square className="card" elevation={0}>
+    <Avatar alt="" src={avatar} />
+    <CardMedia className="media" component="img" image={media} title="" />
+    <CardContent className="cardText">
+      <Typography className="title" component="h2" variant="subtitle2">
+        {title}
+      </Typography>
+    </CardContent>
+  </Card>
+);
+
 // TODO: Add alt to avatar and title to image. Create in mockdata.
 
 export default Tip;
