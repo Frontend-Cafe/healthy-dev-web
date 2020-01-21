5779838cc68d78b1f9ea63fdf6e0c67137832b1a
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-21 19:21:44 -0300

    Removed return from comps. Cleared margin.

--- src/components/ListTips/TipList.jsx
+++ src/components/ListTips/TipList.jsx
@@ -2,18 +2,14 @@ import React from "react";
 
 import Tip from "../Tip/Tip.jsx";
 
-const TipList = ({ tipStates }) => {
-  return (
-    <div className="grid-container">
-      {tipStates.map((tipState, index) => {
-        return (
-          <div key={tipState.id} className={`card card-${index}`}>
-            <Tip tipState={tipState} />;
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+const TipList = ({ tipStates }) => (
+  <div className="grid-container">
+    {tipStates.map((tipState, index) => (
+      <div key={tipState.id} className={`card card-${index}`}>
+        <Tip tipState={tipState} />;
+      </div>
+    ))}
+  </div>
+);
 
 export default TipList;
