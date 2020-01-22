14f6efd329afdf263e276af29a265d1e121170d8
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-21 21:17:59 -0300

    Fixed Tipstate destructuring.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import TipsLayout from "components/TipsLayout/TipsLayout";
 import tipStates from "./lib/mockData";
 import "./App.scss";
 import { theme } from "./styles/theme";
-import TipList from "./components/ListTips/TipList.jsx";
+import TipList from "./components/TipList/TipList.jsx";
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <div className="App">
       <TipsLayout sectionTitle="Fitness Tips" />
+      <Router>
+        <Route exact path="/" />
+        <TipList tipStates={tipStates} />
+      </Router>
     </div>
   </ThemeProvider>
 );
