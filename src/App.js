a365bd614a2552a3e38045e429526b245a65be99
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-23 01:48:40 -0300

    Added import comments. PR corrections

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import TipsLayout from "components/TipsLayout/TipsLayout";
 import { ROUTES } from "lib/routes";
 import tipStates from "lib/mockData";
 // Style
-import "App.scss";
+import "./App.scss";
 import { theme } from "styles/theme";
 // Components
 import TipList from "components/TipList/TipList";
+import TipsLayout from "components/TipsLayout/TipsLayout";
 
 const App = () => (
   <ThemeProvider theme={theme}>
