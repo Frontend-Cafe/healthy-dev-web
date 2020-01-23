cd6b7c0e9db9f95d9266c5f87221c2a855eda29d
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-23 01:18:56 -0300

    Changed imports to absolute. Defined scr in jsconfig file.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TipsLayout from "components/TipsLayout/TipsLayout";
 import { ROUTES } from "lib/routes";
 import tipStates from "lib/mockData";
 // Style
-import "./App.scss";
+import "App.scss";
 import { theme } from "styles/theme";
 // Components
 import TipList from "components/TipList/TipList";
