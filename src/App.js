a07e7585f5c068f95c60e82207acd1397d37a910
Maximiliano Blanco <inkcoming@protonmail.com>
2020-01-23 01:48:40 -0300

    Added import comments. PR corrections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Hooks
 import { ThemeProvider } from "@material-ui/core";
+// Data
 import { ROUTES } from "lib/routes";
 import tipStates from "lib/mockData";
+// Style
 import "./App.scss";
 import { theme } from "styles/theme";
+// Components
 import TipList from "components/TipList/TipList";
 
 const App = () => (
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,3 +1,4 @@
+// Hooks
 import { createMuiTheme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
