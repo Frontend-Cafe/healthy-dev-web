fd005a14b01c8b3cff4eb20712950cef4c0d1e80
Victor Escalona <victorescalonag@gmail.com>
2020-01-23 21:07:55 -0500

    error 2.0

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -15,65 +15,67 @@ import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import { makeStyles } from "@material-ui/core/styles";
 
 const Login = () => {
-    const useStyles = makeStyles({
-        inputInfo: {
-            width: "100%",
-            height: "3rem",
-        },
-    });
+  const useStyles = makeStyles({
+    inputInfo: {
+      width: "100%",
+      height: "3rem"
+    }
+  });
 
-    const [values, setValues] = React.useState({
-        amount: "",
-        password: "",
-        weight: "",
-        weightRange: "",
-        showPassword: false,
-    });
+  const [values, setValues] = React.useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false
+  });
 
-    const handleClickShowPassword = () => {
-        setValues({ ...values, showPassword: !values.showPassword });
-    };
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
 
-    const handleMouseDownPassword = event => {
-        event.preventDefault();
-    };
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
 
-    const handleChange = prop => event => {
-        setValues({ ...values, [prop]: event.target.value });
-    };
-    const classes = useStyles();
-    return (
-        <div className="container">
-            <div className="title_login">
-                Bienvenide a <br /> Healthy Dev App
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const classes = useStyles();
+  return (
+    <div className="container">
+      <div className="title_login">
+        Bienvenide a <br /> Healthy Dev App
       </div>
-            <div className="login_form">
-                <form action="">
-                    <FormControl className={classes.inputInfo} variant="filled">
-                        <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
-                        <FilledInput
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        edge="end"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                    >
-                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            id="filled-adornment-password"
-                            type={values.showPassword ? "text" : "password"}
-                            value={values.password}
-                            onChange={handleChange("password")}
-                        />
-                    </FormControl>
-                </form>
-            </div>
-        </div>
-    );
+      <div className="login_form">
+        <form action="">
+          <FormControl className={classes.inputInfo} variant="filled">
+            <InputLabel htmlFor="filled-adornment-password">
+              Password
+            </InputLabel>
+            <FilledInput
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    edge="end"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              id="filled-adornment-password"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange("password")}
+            />
+          </FormControl>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
