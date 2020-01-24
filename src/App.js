78f20f587158f52c87dce82ef908ff74e7dcca87
Victor Escalona <victorescalonag@gmail.com>
2020-01-23 21:07:10 -0500

    error

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import PATH from "./constants/paths";
 import Login from "./pages/Login/Login";
-import loginMaterial from "./pages/Login/LoginMaterial";
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Route exact path={PATH.home} />
         <Route exact component={Login} path={PATH.login} />
-        <Route exact component={loginMaterial} path="/loginm" />
       </Router>
     </div>
   );
