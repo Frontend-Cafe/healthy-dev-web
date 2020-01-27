0ebadb589cd1812ded2d8e7d68c39960c791266e
Brian Durand <briandurand1990@gmail.com>
2020-01-27 20:53:08 -0300

    Return implicito CarrouselProfile

--- src/components/CarrouselProfile/index.js
+++ src/components/CarrouselProfile/index.js
@@ -6,19 +6,17 @@ import CardCarrousel from "components/CardCarrousel";
 //Imports Styles
 import "./styles.scss";
 
-const CarrouselProfile = ({ titleSection, items }) => {
-  return (
-    <Box className="carrousel-profile" component="article">
-      <Box component="header">
-        <Typography component="h2">{titleSection}</Typography>
-      </Box>
-      <Box>
-        {items.map(item => (
-          <CardCarrousel key={item.id} image={item.img} title={item.title} />
-        ))}
-      </Box>
+const CarrouselProfile = ({ titleSection, items }) => (
+  <Box className="carrousel-profile" component="article">
+    <Box component="header">
+      <Typography component="h2">{titleSection}</Typography>
     </Box>
-  );
-};
+    <Box>
+      {items.map(item => (
+        <CardCarrousel key={item.id} image={item.img} title={item.title} />
+      ))}
+    </Box>
+  </Box>
+);
 
 export default CarrouselProfile;
