439d7f959da2df1ceef34766224284cc572fbb69
Santiago <santiagoharkes@gmail.com>
2020-01-28 15:33:30 -0300

    Adding error and loading components

---
+++ src/components/Loading/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.scss";
+
+export default function Loading() {
+  return (
+    <div className="loadingContainer">
+      <div className="ballOne" />
+      <div className="ballTwo" />
+      <div className="ballThree" />
+    </div>
+  );
+}
