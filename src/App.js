fd22878a8599570d0a490b9d0a9dc771af77a1d4
Maximiliano Blanco <inkcoming@protonmail.com>
2020-02-10 17:29:17 -0300

    Removed fetch changed App.js back.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 // Hooks
 import { ThemeProvider } from "@material-ui/core";
-// Tip data from mocky API, uncomment if offline
-// import tipStates from "lib/mockData";
+// Tip data from mocky API: "http://www.mocky.io/v2/5e2d10a43000005c00e77c8b"
+import tipStates from "lib/mockData";
 // Style
 import "./App.scss";
 import { theme } from "styles/theme";
@@ -10,33 +10,14 @@ import { theme } from "styles/theme";
 import TipList from "components/TipList/TipList";
 import TipsLayout from "components/TipsLayout/TipsLayout";
 
-const App = () => {
-  const [tipStates, setTipStates] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const handleError = err => {
-        console.log("Fetch Error", err);
-      };
-      const endpoint = "http://www.mocky.io/v2/5e2d10a43000005c00e77c8b";
-      const data = await fetch(endpoint).catch(handleError);
-      const tipStates = await data.json();
-
-      setTipStates(tipStates);
-      localStorage.setItem("tipStates", JSON.stringify(tipStates));
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <TipsLayout sectionTitle="Fitness Tips">
-          <TipList tipStates={tipStates} />
-        </TipsLayout>
-      </div>
-    </ThemeProvider>
-  );
-};
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <div className="App">
+      <TipsLayout sectionTitle="Fitness Tips">
+        <TipList tipStates={tipStates} />
+      </TipsLayout>
+    </div>
+  </ThemeProvider>
+);
 
 export default App;
