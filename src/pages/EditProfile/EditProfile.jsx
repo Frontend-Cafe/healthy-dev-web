e509b209d642a1068c14eed10c1d732ebb546a40
Maximiliano Blanco <inkcoming@protonmail.com>
2020-02-15 23:24:43 -0300

    Destructured useFormik.

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -25,7 +25,7 @@ const EditProfile = ({ user, avatar }) => {
   const [showNewPass, setShowNewPass] = useState(false);
   const [showConfirmPass, setShowConfirmPass] = useState(false);
   console.log(avatar);
-  const formik = useFormik({
+  const { values, errors, touched, isSubmitting, handleBlur, handleChange, handleSubmit } = useFormik({
     initialValues: {
       name: user || "",
       email: "",
@@ -35,10 +35,9 @@ const EditProfile = ({ user, avatar }) => {
       showConfirmPass: showConfirmPass,
     },
     validationSchema: EditProfileSchema,
-    handleSubmit(values) {
+    handleSubmit(values, { resetForm }) {
       console.log(values);
-      document.getElementById("newPassword").value = "";
-      document.getElementById("confirmNewPassword").value = "";
+      resetForm();
     },
     validateOnChange: false,
   });
@@ -49,7 +48,7 @@ const EditProfile = ({ user, avatar }) => {
 
   return (
     <Container className="outer-container">
-      <form className="inner-container" onSubmit={formik.handleSubmit}>
+      <form className="inner-container" onSubmit={handleSubmit}>
         <Grid container alignItems="center" direction="row" justify="center">
           <Grid item className="title-item">
             <Typography variant="h1">Editar Perfil</Typography>
@@ -60,17 +59,17 @@ const EditProfile = ({ user, avatar }) => {
               fullWidth
               required
               aria-describedby="Nombre"
-              error={formik.errors.name && formik.touched.name}
+              error={errors.name && touched.name}
               id="name"
               name="name"
               placeholder="Nombre"
-              value={formik.values.name}
+              value={values.name}
               variant="filled"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
+              onBlur={handleBlur}
+              onChange={handleChange}
             />
-            <FormHelperText error={formik.errors.name && formik.touched.name} variant="filled">
-              {formik.errors.name && formik.touched.name}
+            <FormHelperText error={errors.name && touched.name} variant="filled">
+              {errors.name && touched.name}
             </FormHelperText>
           </Grid>
           <Grid item className="email-item">
@@ -78,17 +77,17 @@ const EditProfile = ({ user, avatar }) => {
               fullWidth
               required
               aria-describedby="Email"
-              error={formik.errors.email && formik.touched.email}
+              error={errors.email && touched.email}
               id="email"
               name="email"
               placeholder="yo@email.com"
-              value={formik.values.email}
+              value={values.email}
               variant="filled"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
+              onBlur={handleBlur}
+              onChange={handleChange}
             />
-            <FormHelperText error={formik.errors.email && formik.touched.email} variant="filled">
-              {formik.errors.email && formik.touched.email}
+            <FormHelperText error={errors.email && touched.email} variant="filled">
+              {errors.email && touched.email}
             </FormHelperText>
           </Grid>
           <Grid item className="password-item">
@@ -96,31 +95,31 @@ const EditProfile = ({ user, avatar }) => {
               fullWidth
               required
               aria-describedby="Cambiar password"
-              error={formik.errors.newPassword && formik.touched.newPassword}
+              error={errors.newPassword && touched.newPassword}
               id="newPassword"
               InputProp={{
-                endAdornment: `
-              <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={() => setShowNewPass(!showNewPass)}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {formik.values.showNewPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              `,
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowNewPass(!showNewPass)}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showNewPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
               }}
               name="newPassword"
               placeholder="Crear Password"
               type={showNewPass ? "text" : "password"}
-              value={formik.values.newPassword}
+              value={values.newPassword}
               variant="filled"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
+              onBlur={handleBlur}
+              onChange={handleChange}
             />
-            <FormHelperText error={formik.errors.newPassword && formik.touched.newPassword} variant="filled">
-              {formik.errors.newPassword && formik.touched.newPassword}
+            <FormHelperText error={errors.newPassword && touched.newPassword} variant="filled">
+              {errors.newPassword && touched.newPassword}
             </FormHelperText>
           </Grid>
           <Grid item className="password-item">
@@ -128,32 +127,31 @@ const EditProfile = ({ user, avatar }) => {
               fullWidth
               required
               aria-describedby="Repetir password"
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={() => setShowConfirmPass(!showConfirmPass)}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {formik.values.showNewPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              error={formik.errors.confirmNewPassword && formik.touched.confirmNewPassword}
+              error={errors.confirmNewPassword && touched.confirmNewPassword}
               id="confirmNewPassword"
+              InputProp={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowConfirmPass(!showConfirmPass)}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showNewPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
               name="confirmNewPassword"
               placeholder="Crear Password"
               type={showConfirmPass ? "text" : "password"}
-              value={formik.values.confirmNewPassword}
+              value={values.confirmNewPassword}
               variant="filled"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
+              onBlur={handleBlur}
+              onChange={handleChange}
             />
-            <FormHelperText
-              error={formik.errors.confirmNewPassword && formik.touched.confirmNewPassword}
-              variant="filled"
-            >
-              {formik.errors.confirmNewPassword && formik.touched.confirmNewPassword}
+            <FormHelperText error={errors.confirmNewPassword && touched.confirmNewPassword} variant="filled">
+              {errors.confirmNewPassword && touched.confirmNewPassword}
             </FormHelperText>
           </Grid>
           <Grid item>
@@ -161,7 +159,7 @@ const EditProfile = ({ user, avatar }) => {
               fullWidth
               className="button"
               color="secondary"
-              disabled={formik.isSubmitting}
+              disabled={isSubmitting}
               type="submit"
               variant="outlined"
             >
